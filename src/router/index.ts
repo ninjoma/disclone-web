f921399595e9542fc6d180bea4a280e9b54dee3f
Ninjam <45521174+ninjoma@users.noreply.github.com>
2022-11-29 18:33:51 +0100

    Fixed Component Exports

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,6 @@ import ServerView from '../views/ServerView.vue';
 import SettingsView from '../views/SettingsView.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/server',
