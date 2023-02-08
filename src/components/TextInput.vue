9dee5ef06acd8d7db492bd7da5efadc455408768
Ninjam <45521174+ninjoma@users.noreply.github.com>
2023-02-08 20:53:21 +0100

    Added Store

---
+++ src/components/TextInput.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    props: {
+        inputType: {
+            type: String,
+            default: "text",
+            inputStyle: String
+        },
+        leftLabel: String,
+        rightLabel: String, 
+        errorLabel: String
+    }
+})
+</script>
+<template> 
+    <div>
+        <label v-if="leftLabel || rightLabel" className="label">
+            <span v-if="leftLabel" className="label-text">{{ leftLabel }}</span>
+            <span v-if="rightLabel" className="label-text-alt"><a class="underline" href="#">{{ rightLabel }}</a></span>
+        </label>
+        <div class="flex">
+            <input :type="inputType" className="input input-bordered flex-1" />
+        </div>
+        <label v-if="errorLabel" className="label">
+            <span v-if="errorLabel" className="label-text">{{ errorLabel }}</span>
+        </label>
+    </div>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,11 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import Router from './router/index';
+import UserStore from './stores/UserStore';
 
 const app = createApp(App);
+
+app.use(UserStore);
 
 // Router
 app.use(Router);
